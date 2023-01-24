80081bc45e45e11e2d435a02be02ffc98e32c248
kunlexi <adesinataofik5@gmail.com>
2023-01-24 11:56:50 +0100

    fresh

--- src/components/GithubUsers.js
+++ src/components/GithubUsers.js
@@ -3,13 +3,30 @@ import { useState, useEffect } from "react";
 const url = "https://api.github.com/users";
 
 const GithubUsers = () => {
-  const [users, setusers] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const getUsers = async () => {
+
+    setIsLoading(true)
+    setIsLoading (false)
+    try {
     const response = await fetch(url);
+    console.log(response);
+    if (!response.ok) {
+        throw new Error("Something went wrong.")
+    }
     const data = await response.json();
-    // console.log(data);
-    setusers(data);
+    setUsers(data);
+    setIsLoading(false)
+
+    } catch (err) {
+        console.log(err.message)
+        setError (true)
+        setIsLoading(false)
+    }
+    
   };
 
   useEffect(() => {
@@ -24,19 +41,29 @@ const GithubUsers = () => {
           <div className="--line"></div>
         </header>
 
+        {isLoading && (
+            <div className="--center-all --p"> 
+            <h4 className="--text-light">Loading...</h4>
+            </div>
+        
+        )}
+
         <div className="--grid-25 --py">
-          {users.map((user) => {
-            const { id, login, avatar_url, html_url } = user;
-            return (
-              <div key={id} className="--card --bg-light --p --flex-start">
-                <img src={avatar_url} alt={login} className="--profile-img" />
-                <span>
-                  <h4>{login}</h4>
-                  <a href={html_url}>View profile</a>
-                </span>
-              </div>
-            );
-          })}
+
+            {error ? (<h4 className="--text-light"> Something went wrong.</h4>) : (
+                users.map((user) => {
+                    const { id, login, avatar_url, html_url } = user;
+                    return (
+                      <div key={id} className="--card --bg-light --p --flex-start">
+                        <img src={avatar_url} alt={login} className="--profile-img" />
+                        <span>
+                          <h4>{login}</h4>
+                          <a href={html_url}>View profile</a>
+                        </span>
+                      </div>
+                    );
+                  })
+            )}
         </div>
       </div>
     </div>
